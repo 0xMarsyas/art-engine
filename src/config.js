e49dfa4bdfb46b17a12bfbfe840b97d55643bb85
0xMarsyas <0xMarsyas@protonmail.com>
2023-01-13 17:45:48 -0500

    Updated config file that explains how to run the Marsyas Generator

--- src/config.js
+++ src/config.js
@@ -11,32 +11,104 @@ const { MODE } = require(path.join(basePath, "src/blendMode.js"));
 const buildDir = path.join(basePath, "/build");
 const layersDir = path.join(basePath, "/layers");
 
-/*********************
- * General Generator Options
- ***********************/
+/*************************************************
+ ************* Generator Options *****************
+ ************************************************/
 
 const description =
-  "This is the description of your NFT project, remember to replace this";
+  "This is the description of your NFT project. Repalce this with an initial description for your project, but this part will be replaced by the Marsyas story generator";
 const baseUri = "ipfs://NewUriToReplace";
 
-const outputJPEG = false; // if false, the generator outputs png's
+// false = the generator outputs png's
+//true = generator outputs JPEGs
+const outputJPEG = false; 
 
 /**
  * Set your tokenID index start number.
  * ⚠️ Be sure it matches your smart contract!
- */
+ * Pls remember ERC-721 contracts start with tokenID 0 :)
+*/ 
 const startIndex = 0;
 
+
+//for best results...
+//make sure your layers are the same width and height as your format output 
 const format = {
   width: 512,
   height: 512,
-  smoothing: true, // set to false when up-scaling pixel art.
+  // set to false when up-scaling pixel art.
+  smoothing: true, 
 };
 
+//generates random color background for your NFT
+//with an opacity control
 const background = {
-  generate: true,
+  generate: false,
   brightness: "80%",
 };
+
+
+//  This is how I would lay out the layers to properly utilize storytelling and proper formatting
+//
+//
+// const layerConfigurations = [
+//   {
+//     growEditionSizeTo: 1500,
+//     namePrefix: "Divine Nymph",
+//     layersOrder: [
+//       { name: "A-Background", trait: "Background"},
+//       { name: "A-Hair_Back", trait: "Hair Back"},
+//       { name: "A-Body", trait: "Skin"},
+//       { name: "A-Body_type", trait: "Body Type"},
+//       { name: "A-Skin_Texture", trait: "Skin Texture" },
+//       { name: "A-Tattoo", trait: "Tattoo"  },
+//       { name: "A-Lips" , trait: "Lips"},
+//       { name: "A-Makeup", opacity: 0.8, trait: "Makeup" },
+//       { name: "A-Eyes", trait: "Eyes"},
+//       { name: "A-Eyebrows", trait: "Eyebrows" },
+//       { name: "A-Clothing_Under", trait: "Clothing Under"},
+//       { name: "A-Body_Jewels", trait: "Body Adornment"},
+//       { name: "A-Clothing_Over", trait: "Clothing Over" },
+//       { name: "A-Hair_Facial", opacity: 0.95, trait: "Facial Hair" },
+//       { name: "A-Hair_Front", trait: "Hair"},
+//       { name: "A-Nose", trait: "Nose"},
+//       { name: "A-Ears", trait: "Ears"},
+//       { name: "A-Hair_Special", trait: "Hair Special"},
+//       { name: "A-Piercing", trait: "Piercing" },
+//       { name: "A-Accessories", trait: "Adornment" },
+//       { name: "A-Over_Face", trait: "Over Face" },
+//       { name: "A-Hair_Strand", trait: "Hair Strand"},
+//       { name: "A-Foreground", trait: "Foreground" },
+//       { name: "A-Type", trait: "Family", bypassDNA: true },
+//       { name: "A-First", trait: "First Name", bypassDNA: true },
+//       { name: "A-Middle", trait: "Middle Name", bypassDNA: true },
+//       { name: "A-Last", trait: "Surname", bypassDNA: true  },
+//       { name: "A-Lucky_Number", trait: "Lucky Number", bypassDNA: true  },
+//       { name: "A-Feels_Level", trait: "Feels Level", bypassDNA: true  },
+//       { name: "A-Power", trait: "Power", bypassDNA: true  },
+//       { name: "A-Essence", trait: "Essence", bypassDNA: true  },
+//       { name: "A-Life_force", trait: "Life Force", bypassDNA: true  },
+//       { name: "A-Zodiac_Birth", trait: "Zodiac Year", bypassDNA: true  },
+//       { name: "A-Zodiac_Sun", trait: "Zodiac Month", bypassDNA: true  },
+//       { name: "A-Zodiac_Moon", trait: "Planet", bypassDNA: true  },
+//       { name: "A-Experience", trait: "Characteristics", bypassDNA: true  },
+//     ],
+//   },
+//
+//
+//
+// If you want to utilize  the resetNameIndex feature of the NFT Chef generator, format like this:
+  // {
+  //   growEditionSizeTo: 10,
+  //   namePrefix: "Lion",
+  //   resetNameIndex: true, // this will start the Lion count at #1 instead of #6
+  //   layersOrder: [
+  //     { name: "Background" },
+  //     { name: "Hats" },
+  //     { name: "Male Hair" },
+  //   ],
+  // },
+
 
 const layerConfigurations = [
   {
@@ -58,16 +130,8 @@ const layerConfigurations = [
       { name: "Shirt Accessories" },
     ],
   },
-  // {
-  //   growEditionSizeTo: 10,
-  //   namePrefix: "Lion",
-  //   resetNameIndex: true, // this will start the Lion count at #1 instead of #6
-  //   layersOrder: [
-  //     { name: "Background" },
-  //     { name: "Hats" },
-  //     { name: "Male Hair" },
-  //   ],
-  // },
+  
+  
 ];
 
 /**
@@ -83,6 +147,8 @@ const debugLogs = true;
  ***********************/
 
 // if you use an empty/transparent file, set the name here.
+// it is possible to set more than one blank file name
+// IE if you want none of the "Skunk_tattoo" traits to show up, put it here as an emptyLayerName and it won't show up in the metadata
 const emptyLayerName = "NONE";
 
 /**
@@ -91,6 +157,8 @@ const emptyLayerName = "NONE";
  * first as the Key, and the incompatible items that _may_ come after.
  * The current version requires all layers to have unique names, or you may
  * accidentally set incompatibilities for the _wrong_ item.
+ * 
+ * This is a very powerful tool you should be using.
  */
 const incompatible = {
   //   Red: ["Dark Long"],
@@ -115,6 +183,9 @@ const forcedCombinations = {
  * In the event that a filename cannot be the trait value name, for example when
  * multiple items should have the same value, specify
  * clean-filename: trait-value override pairs. Wrap filenames with spaces in quotes.
+ * 
+ * * This is another very powerful tool you should be using.
+ * 
  */
 const traitValueOverrides = {
   Helmet: "Space Helmet",
@@ -124,28 +195,14 @@ const traitValueOverrides = {
 const extraMetadata = {};
 
 const extraAttributes = () => [
-  // Optionally, if you need to overwrite one of your layers attributes.
-  // You can include the same name as the layer, here, and it will overwrite
   //
-  // {
-  // trait_type: "Bottom lid",
-  //   value: ` Bottom lid # ${Math.random() * 100}`,
-  // },
-  // {
-  //   display_type: "boost_number",
-  //   trait_type: "Aqua Power",
-  //   value: Math.random() * 100,
-  // },
-  // {
-  //   display_type: "boost_number",
-  //   trait_type: "Health",
-  //   value: Math.random() * 100,
-  // },
-  // {
-  //   display_type: "boost_number",
-  //   trait_type: "Mana",
-  //   value: Math.floor(Math.random() * 100),
-  // },
+  //
+  // Note from Marsyas - I never personally found benefit from this section and preferred
+  // to use the blank png method I outined in the readme file
+  //
+  //
+  //
+
 ];
 
 // Outputs an Keccack256 hash for the image. Required for provenance hash
